59340c3951a93c0713827d0972303da3fb972737
Eric D Helms <ehelms@redhat.com>
2013-02-26 00:21:38 -0500

    Adding bowerrc and the start of a new dependency structure.

--- lib/alchemy/engine.rb
+++ lib/alchemy/engine.rb
@@ -4,5 +4,8 @@ module Alchemy
   class Engine < ::Rails::Engine
     isolate_namespace Alchemy
 
+    initializer "alchemy.assets.paths" do |app|
+      app.config.assets.paths << "#{self.root}/vendor/assets"
+    end
   end
 end
