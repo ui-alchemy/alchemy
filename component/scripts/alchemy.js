111f476aba53355038935c8ad3ad97e72abe4109
Eric D. Helms <ericdhelms@gmail.com>
2013-05-05 11:11:46 -0400

    Adding JsDoc.

--- component/scripts/alchemy.js
+++ component/scripts/alchemy.js
@@ -1,4 +1,12 @@
 'use strict';
 
+// Defines the base Alchemy templates module for housing cached versions of component
+// templates within JavaScript.
+// 
+// @module alch-templates
 angular.module('alch-templates', []);
+
+// Base Alchemy module for components to link up to.
+//
+// @module alchemy
 angular.module('alchemy', ['alch-templates']);
--- component/scripts/onenter.directive.js
+++ component/scripts/onenter.directive.js
@@ -1,5 +1,13 @@
 'use strict';
 
+// Defines an onEnter directive for handling the enter key being pressed
+// on an element. Accepts any expression and executes that expression
+// as is.
+//
+// @example <input type="text" ng-model="name" on-enter="enterPressed(name)">
+//
+// @module alchemy
+// @directive onEnter
 angular.module('alchemy').directive('onEnter', function() {
     return {
         scope: true,
